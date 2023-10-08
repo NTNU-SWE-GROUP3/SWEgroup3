efbbb82d92cb9cd96d46bcdf4365b6a2d311b7f3
IvanChang0290 <ivanty2chang@gmail.com>
2023-10-08 23:51:17 +0800

    added ui_template

---
+++ unity_UI/UI_template/Assets/Level_loader.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_loader : MonoBehaviour
+{
+
+    // Update is called once per frame
+    /*
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Load_next_level();
+        }
+
+        
+    }
+
+    public void Load_next_level()
+    {
+        SceneManger.LoadScene(1);
+    }*/
+}
