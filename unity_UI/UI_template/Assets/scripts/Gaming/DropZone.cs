2f7c2575592b9e4276f76999a7b24dbd4730fad3
Chianghaoyu <a0981610711@gmail.com>
2023-10-17 23:57:57 +0800

    Add DragCard.cs and DropZone.cs
    Prefabs/GameScene/Card - add component Drag Card (Script) and Canvas Group
    
    Prefabs/GameScene/PlayerShow - add component Drop Zone (Script)

---
+++ unity_UI/UI_template/Assets/scripts/Gaming/DropZone.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour,IDropHandler
+{
+    public static bool backToHand = true;
+    public static bool haveCard = false;
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag.GetComponent<CardDisplay>().cardBack == false)
+        {
+            if(haveCard)
+            {
+                Debug.Log("There's a card already.");
+            }
+            else
+            {
+                Debug.Log(eventData.pointerDrag.name + "was dropped on " + gameObject.name);
+            
+                if(eventData.pointerDrag != null)
+                {
+                    eventData.pointerDrag.GetComponent<RectTransform>().position = GetComponent<RectTransform>().position;
+                    backToHand = false;
+                    haveCard = true;
+                }
+            }
+        }
+        
+        
+        
+    }
+}
