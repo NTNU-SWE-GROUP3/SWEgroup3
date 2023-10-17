70b30e7db5a1de3b6cb00aa02d230aed93a36704
brian098091 <93636904+brian098091@users.noreply.github.com>
2023-10-17 12:51:47 +0800

    adjust card position

--- unity_UI/UI_template/Assets/scripts/Gaming/ComputerPlayer.cs
+++ unity_UI/UI_template/Assets/scripts/Gaming/ComputerPlayer.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public class ComputerPlayer : MonoBehaviour
 {
-    public CardDatabase cardDatabase;
+    // public CardDatabase cardDatabase;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayRandomCard();
-    }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     PlayRandomCard();
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    // }
 
-    public void PlayCard(Card SelectedCard)
-    {
-        //List<Card> playerHand = 
-        //playerHand.Remove(selectedCard);
+    // public void PlayCard(Card SelectedCard)
+    // {
+    //     //List<Card> playerHand = 
+    //     //playerHand.Remove(selectedCard);
         
-    }
-    public void PlayRandomCard()
-    {
-        if (cardDatabase != null && cardDatabase.cardList.Count > 0)
-        {
-            int randomIndex = Random.Range(0, cardDatabase.cardList.Count);
-            Card randomCard = cardDatabase.cardList[randomIndex];
+    // }
+    // public void PlayRandomCard()
+    // {
+    //     if (cardDatabase != null && cardDatabase.cardList.Count > 0)
+    //     {
+    //         int randomIndex = Random.Range(0, cardDatabase.cardList.Count);
+    //         Card randomCard = cardDatabase.cardList[randomIndex];
 
-            // 出牌
-            PlayCard(randomCard);
-        }
-    }
+    //         // 出牌
+    //         PlayCard(randomCard);
+    //     }
+    // }
 }
