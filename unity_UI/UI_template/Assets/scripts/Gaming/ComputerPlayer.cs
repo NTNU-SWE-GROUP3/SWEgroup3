586208c3267ac54844a8fbe5124f6fe5ee9363dc
kuwilson2001 <kuwilson0922@gmail.com>
2023-10-18 18:45:05 +0800

    Add ComputerPlayer

--- unity_UI/UI_template/Assets/scripts/Gaming/ComputerPlayer.cs
+++ unity_UI/UI_template/Assets/scripts/Gaming/ComputerPlayer.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 
 public class ComputerPlayer : MonoBehaviour
 {
-<<<<<<< HEAD
     CardDatabase cardDatabase;
-=======
-    // public CardDatabase cardDatabase;
->>>>>>> e2ccb93d1b977a132cc7cc9f91107c424728d35b
     
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     PlayRandomCard();
-    // }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayRandomCard();
+    }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    // }
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
-<<<<<<< HEAD
     public void PlayCard(Card SelectedCard)
     {
-        // 在這裡執行出牌的代碼
-        // 例如，你可以在這裡使用 SelectedCard 做一些事情
+        // 出牌
     }
 
     public void PlayRandomCard()
@@ -34,23 +28,8 @@ public class ComputerPlayer : MonoBehaviour
         {
             int randomIndex = Random.Range(0, CardDatabase.cardList.Count);
             Card randomCard = CardDatabase.cardList[randomIndex];
-=======
-    // public void PlayCard(Card SelectedCard)
-    // {
-    //     //List<Card> playerHand = 
-    //     //playerHand.Remove(selectedCard);
-        
-    // }
-    // public void PlayRandomCard()
-    // {
-    //     if (cardDatabase != null && cardDatabase.cardList.Count > 0)
-    //     {
-    //         int randomIndex = Random.Range(0, cardDatabase.cardList.Count);
-    //         Card randomCard = cardDatabase.cardList[randomIndex];
->>>>>>> e2ccb93d1b977a132cc7cc9f91107c424728d35b
 
-    //         // 出牌
-    //         PlayCard(randomCard);
-    //     }
-    // }
+            PlayCard(randomCard);
+        }
+    }
 }
