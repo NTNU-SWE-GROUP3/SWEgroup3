717fe779f5c7121b9d6f18d192f96502909fabbb
hsiaooooooo <alllllvin21292@gmail.com>
2023-10-21 13:52:21 +0800

    fix ButtonAction.cs warning

--- unity_UI/UI_template/Assets/scripts/gacha/ButtonAction.cs
+++ unity_UI/UI_template/Assets/scripts/gacha/ButtonAction.cs
@@ -38,7 +38,7 @@ public class ButtonAction : MonoBehaviour
         UnityWebRequest www = UnityWebRequest.Post(apiUrl, form);
         yield return www.SendWebRequest();
 
-        if (www.isNetworkError || www.isHttpError)
+        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
         {
             Debug.Log("failed");
             Debug.LogError(www.error);
