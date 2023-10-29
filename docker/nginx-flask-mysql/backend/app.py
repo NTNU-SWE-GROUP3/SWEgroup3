37c42a116b486f6868e94e2511fc2d6221114c11
yuji2535 <72071039+yuji2535@users.noreply.github.com>
2023-10-29 21:29:56 +0800

    Improved debugging: hot reload enabled

--- docker/nginx-flask-mysql/backend/app.py
+++ docker/nginx-flask-mysql/backend/app.py
@@ -7,7 +7,7 @@ class DBManager:
     def __init__(self, database='swegroup3game', host="db", user="root", password_file=None):
         pf = open(password_file, 'r')
         self.connection = mysql.connector.connect(
-            user=user, 
+            user=user,
             password=pf.read(),
             host=host, # name of the mysql service as set in the docker compose file
             database=database,
@@ -15,13 +15,13 @@ class DBManager:
         )
         pf.close()
         self.cursor = self.connection.cursor()
-    
+
     def populate_db(self):
         self.cursor.execute('DROP TABLE IF EXISTS blog')
         self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
         self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
         self.connection.commit()
-    
+
     def query_titles(self):
         self.cursor.execute('SELECT title FROM blog')
         rec = []
@@ -46,6 +46,18 @@ def listBlog():
         response = response  + '<div>   Hello  ' + c + '</div>'
     return response
 
+@server.route('/test')
+def listBlogTest():
+    global conn
+    if not conn:
+        conn = DBManager(password_file='/run/secrets/db-password')
+    rec = conn.query_titles()
+
+    response = ''
+    for c in rec:
+        response = response  + '<div>   AI' + c + '</div>'
+    return response
 
 if __name__ == '__main__':
-    server.run()
+    # server.run()
+    server.run(debug=True,host='0.0.0.0')
