a50cdb49b7f2264c219976a8bf970df639aab1cd
hsiaooooooo <alllllvin21292@gmail.com>
2023-11-15 23:06:34 +0800

    set input constraint

--- unity_UI/UI_template/Assets/scripts/gacha/PurchaseController.cs
+++ unity_UI/UI_template/Assets/scripts/gacha/PurchaseController.cs
@@ -9,13 +9,25 @@ namespace PurchaseControl
     {
         [SerializeField] GameObject purchasePanel;
         [SerializeField] InputField[] inputFields;
-        public Dropdown dropdownRegion;
-        string[] allOptions = { "Japan", "Taiwan", "Korea", "China", "Germany", "Italy", "France", "Spain", "Canada" };
+        [SerializeField] Dropdown dropdownRegion;
         public Action actionReferences;
+        string[] allOptions = { "Japan", "Taiwan", "Korea", "China", "Germany", "Italy", "France", "Spain", "Canada" };
+        Text messageText;
 
         void Start()
         {
             DropdownInit();
+            InputFeildsInit();
+            MessageInit();
+        }
+
+        public void OpenPurchasePanel()
+        {
+            purchasePanel.SetActive(true);
+            actionReferences.buyClicked = false;
+            actionReferences.cancelClicked = false;
+            ClearInputFields();
+            ClearMessages();
         }
 
         void DropdownInit()
@@ -40,6 +52,28 @@ namespace PurchaseControl
             }
         }
 
+        void InputFeildsInit()
+        {
+            if (inputFields != null)
+            {
+                inputFields[0].characterLimit = 12;
+                inputFields[0].contentType = InputField.ContentType.IntegerNumber;
+
+                inputFields[1].characterLimit = 4;
+                inputFields[1].contentType = InputField.ContentType.IntegerNumber;
+
+                inputFields[2].characterLimit = 3;
+                inputFields[2].contentType = InputField.ContentType.IntegerNumber;
+
+                inputFields[4].characterLimit = 5;
+                inputFields[4].contentType = InputField.ContentType.IntegerNumber;
+            }
+            else
+            {
+                Debug.Log("Failed to get input field.");
+            }
+        }
+        
         void ClearInputFields()
         {
             foreach (InputField inputField in inputFields)
@@ -51,12 +85,39 @@ namespace PurchaseControl
             }
         }
 
-        public void OpenPurchasePanel()
+        void ClearMessages()
         {
-            purchasePanel.SetActive(true);
-            actionReferences.buyClicked = false;
-            actionReferences.cancelClicked = false;
-            ClearInputFields();
+            if (messageText != null)
+            {
+                messageText.text = "";
+            }
+            else
+            {
+                Debug.Log("Can't find message text.");
+            }
+        }
+
+        void MessageInit()
+        {
+            messageText = purchasePanel.transform.Find("Message").GetComponent<Text>();
+            if (messageText == null)
+            {
+                Debug.Log("Cannot find Text under purchase panel.");
+
+            }
+            else
+            {
+                HideMessage();
+            }
+
+        }
+
+        void HideMessage()
+        {
+            if (messageText != null)
+            {
+                messageText.gameObject.SetActive(false);
+            }
         }
 
     }
