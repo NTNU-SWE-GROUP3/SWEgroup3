c67e5a5c80f2853c67793fc8ea64d8b70c0de171
yuji2535 <72071039+yuji2535@users.noreply.github.com>
2023-11-21 17:30:07 +0800

    new: log settings added settings of generating app.log as a log file.

--- docker/nginx-flask-mysql/backend/app.py
+++ docker/nginx-flask-mysql/backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_mail import Mail
 from account import account
 from forget_password import forget_password
+import logging
 
 server = Flask(__name__)
 
@@ -17,6 +18,13 @@ server.config.update(
     MAIL_PASSWORD='xybcnshquazoqoux'
 )
 mail = Mail(server)
+LOGFILE = "app.log"
+server.logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler(LOGFILE)
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+server.logger.addHandler(fh)
 
 # =================
 # test: http GET
