755c638da73157b40c95a04877dda25ef38da50d
hsiaooooooo <136319309+hsiaooooooo@users.noreply.github.com>
2023-11-29 12:23:50 +0800

    add get account equipped card_style

--- server/game.py
+++ server/game.py
@@ -36,6 +36,21 @@ def GetSkills():
         return jsonify({"skills": "Invalid token id"}), 400
 
 
+@gaming.route("/get_card_styles", methods=["POST"])
+def GetCardStyles():
+    try:
+        token_id = request.form.get("token_id")
+        account_id = GetAccountId(token_id)
+
+        if account_id is not None:
+            card_styles = GetEquippedCardStyles(account_id)
+            return jsonify({"card_styles": card_styles})
+        else:
+            return jsonify({"card_styles": "Invalid token_id"}), 400
+
+    except Exception as e:
+        return jsonify({"card_styles": "Invalid token id"}), 400
+
 def GetAccountId(token_id):
     try:
         connection = create_mysql_connection()
@@ -69,4 +84,21 @@ def GetEquippedSkills(account_id):
         return skills
     except Exception as e:
         print("Error in get_player_skills:", e)
+        return []
+    
+def GetEquippedCardStyles(account_id):
+    try:
+        connection = create_mysql_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        cursor.execute("SELECT card_style_id FROM account_card_style WHERE account_id = %s AND equip_status = 1", (account_id,))
+        results = cursor.fetchall()
+
+        connection.close()
+
+        card_styles = [result["card_style_id"] for result in results]
+        print(card_styles)
+        return card_styles
+    except Exception as e:
+        print("Error in getEquippedCardStyle:", e)
         return []
