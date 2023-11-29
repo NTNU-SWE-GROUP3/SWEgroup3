1f76ff5e6634bde6f65f826c093d63c2d52b14d9
hsiaooooooo <136319309+hsiaooooooo@users.noreply.github.com>
2023-11-29 11:54:30 +0800

    add get user equipped skill

--- server/game.py
+++ server/game.py
@@ -1,7 +1,8 @@
 import mysql.connector
 from flask import Flask, request, jsonify, Blueprint
 
-gaming = Blueprint('gaming',__name__, url_prefix='/gaming')
+gaming = Blueprint("gaming", __name__, url_prefix="/gaming")
+
 
 db_config = {
     "host": "localhost",
@@ -10,9 +11,62 @@ db_config = {
     "database": "game",
 }
 
+
 def create_mysql_connection():
-    return mysql.connector.connect(**db_config,autocommit=True)
+    return mysql.connector.connect(**db_config, autocommit=True)
 
-@gaming.route('/get_skills',method=['POST'])
+@gaming.route("/")
+def index():
+    return 200
+
+@gaming.route("/get_skills", methods=["POST"])
 def GetSkills():
+    try:
+        token_id = request.form.get("token_id")
+        account_id = GetAccountId(token_id)
 
+        if account_id is not None:
+            skills = GetEquippedSkills(account_id)
+            return jsonify({"skills": skills})
+        else:
+            return jsonify({"skills": "Invalid token_id"}), 400
+
+
+    except Exception as e:
+        return jsonify({"skills": "Invalid token id"}), 400
+
+
+def GetAccountId(token_id):
+    try:
+        connection = create_mysql_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        cursor.execute("SELECT id FROM account WHERE token_id = %s", (token_id,))
+        result = cursor.fetchone()
+
+        connection.close()
+
+        if result:
+            return result["id"]
+        else:
+            return None
+    except Exception as e:
+        print("Error in get_account_id:", e)
+        return None
+
+def GetEquippedSkills(account_id):
+    try:
+        connection = create_mysql_connection()
+        cursor = connection.cursor(dictionary=True)
+
+        cursor.execute("SELECT skill_id FROM account_skill WHERE account_id = %s AND equip_status = 1", (account_id,))
+        results = cursor.fetchall()
+
+        connection.close()
+
+        skills = [result["skill_id"] for result in results]
+        print(skills)
+        return skills
+    except Exception as e:
+        print("Error in get_player_skills:", e)
+        return []
