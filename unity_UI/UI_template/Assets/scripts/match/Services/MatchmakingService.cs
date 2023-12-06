5ed5a76295d0216fc25a600514d673aaced1503d
Melo <thomas20030413@gmail.com>
2023-12-06 11:34:59 +0800

    Update MatchmakingService.cs
    Add Constatns class, Lobbydata structure and modify QueryFilter.

--- unity_UI/UI_template/Assets/scripts/match/Services/MatchmakingService.cs
+++ unity_UI/UI_template/Assets/scripts/match/Services/MatchmakingService.cs
@@ -9,7 +9,8 @@ using Unity.Services.Relay;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
-public static class MatchmakingService {
+public static class MatchmakingService
+{
     private const int HeartbeatInterval = 15;
     private const int LobbyRefreshRate = 2; // Rate limits at 2
 
@@ -18,15 +19,18 @@ public static class MatchmakingService {
     private static Lobby _currentLobby;
     private static CancellationTokenSource _heartbeatSource, _updateLobbySource;
 
-    private static UnityTransport Transport {
+    private static UnityTransport Transport
+    {
         get => _transport != null ? _transport : _transport = Object.FindObjectOfType<UnityTransport>();
         set => _transport = value;
     }
 
     public static event Action<Lobby> CurrentLobbyRefreshed;
 
-    public static void ResetStatics() {
-        if (Transport != null) {
+    public static void ResetStatics()
+    {
+        if (Transport != null)
+        {
             Transport.Shutdown();
             Transport = null;
         }
@@ -34,15 +38,15 @@ public static class MatchmakingService {
         _currentLobby = null;
     }
 
-    // Obviously you'd want to add customization to the query, but this
-    // will suffice for this simple demo
-    public static async Task<List<Lobby>> GatherLobbies() {
-        var options = new QueryLobbiesOptions {
+    public static async Task<List<Lobby>> GatherLobbies(LobbyData data)
+    {
+        var options = new QueryLobbiesOptions
+        {
             Count = 15,
-
             Filters = new List<QueryFilter> {
                 new(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT),
-                new(QueryFilter.FieldOptions.IsLocked, "0", QueryFilter.OpOptions.EQ)
+                new(QueryFilter.FieldOptions.IsLocked, "0", QueryFilter.OpOptions.EQ),
+                new(QueryFilter.FieldOptions.N1, data.Type.ToString(), QueryFIlter.OpOptions.EQ)
             }
         };
 
@@ -50,13 +54,14 @@ public static class MatchmakingService {
         return allLobbies.Results;
     }
 
-    public static async Task CreateLobbyWithAllocation(LobbyData data) {
+    public static async Task CreateLobbyWithAllocation(LobbyData data)
+    {
         // Create a relay allocation and generate a join code to share with the lobby
         var a = await RelayService.Instance.CreateAllocationAsync(data.MaxPlayers);
         var joinCode = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
-
         // Create a lobby, adding the relay join code to the lobby data
-        var options = new CreateLobbyOptions {
+        var options = new CreateLobbyOptions
+        {
             Data = new Dictionary<string, DataObject> {
                 { Constants.JoinKey, new DataObject(DataObject.VisibilityOptions.Member, joinCode) },
                 { Constants.GameTypeKey, new DataObject(DataObject.VisibilityOptions.Public, data.Type.ToString(), DataObject.IndexOptions.N1) }, {
@@ -66,7 +71,7 @@ public static class MatchmakingService {
             }
         };
 
-        _currentLobby = await Lobbies.Instance.CreateLobbyAsync( "New", data.MaxPlayers, options);
+        _currentLobby = await Lobbies.Instance.CreateLobbyAsync("New", data.MaxPlayers, options);
 
         Transport.SetHostRelayData(a.RelayServer.IpV4, (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData);
 
@@ -74,24 +79,30 @@ public static class MatchmakingService {
         PeriodicallyRefreshLobby();
     }
 
-    public static async Task LockLobby() {
-        try {
+    public static async Task LockLobby()
+    {
+        try
+        {
             await Lobbies.Instance.UpdateLobbyAsync(_currentLobby.Id, new UpdateLobbyOptions { IsLocked = true });
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
             Debug.Log($"Failed closing lobby: {e}");
         }
     }
 
-    private static async void Heartbeat() {
+    private static async void Heartbeat()
+    {
         _heartbeatSource = new CancellationTokenSource();
-        while (!_heartbeatSource.IsCancellationRequested && _currentLobby != null) {
+        while (!_heartbeatSource.IsCancellationRequested && _currentLobby != null)
+        {
             await Lobbies.Instance.SendHeartbeatPingAsync(_currentLobby.Id);
             await Task.Delay(HeartbeatInterval * 1000);
         }
     }
 
-    public static async Task JoinLobbyWithAllocation(string lobbyId) {
+    public static async Task JoinLobbyWithAllocation(string lobbyId)
+    {
         _currentLobby = await Lobbies.Instance.JoinLobbyByIdAsync(lobbyId);
         var a = await RelayService.Instance.JoinAllocationAsync(_currentLobby.Data[Constants.JoinKey].Value);
 
@@ -101,7 +112,8 @@ public static class MatchmakingService {
     }
 
     // This function is used for Friend game X join, but with problem....
-    public static async Task JoinLobbyWithAllocationCode(string lobbyCode) {
+    public static async Task JoinLobbyWithAllocationCode(string lobbyCode)
+    {
         _currentLobby = await Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCode);
         var a = await RelayService.Instance.JoinAllocationAsync(_currentLobby.Data[Constants.JoinKey].Value);
 
@@ -110,28 +122,59 @@ public static class MatchmakingService {
         PeriodicallyRefreshLobby();
     }
 
-    private static async void PeriodicallyRefreshLobby() {
+    private static async void PeriodicallyRefreshLobby()
+    {
         _updateLobbySource = new CancellationTokenSource();
         await Task.Delay(LobbyRefreshRate * 1000);
-        while (!_updateLobbySource.IsCancellationRequested && _currentLobby != null) {
+        while (!_updateLobbySource.IsCancellationRequested && _currentLobby != null)
+        {
             _currentLobby = await Lobbies.Instance.GetLobbyAsync(_currentLobby.Id);
             CurrentLobbyRefreshed?.Invoke(_currentLobby);
             await Task.Delay(LobbyRefreshRate * 1000);
         }
     }
 
-    public static async Task LeaveLobby() {
+    public static async Task LeaveLobby()
+    {
         _heartbeatSource?.Cancel();
         _updateLobbySource?.Cancel();
 
         if (_currentLobby != null)
-            try {
+            try
+            {
                 if (_currentLobby.HostId == Authentication.PlayerId) await Lobbies.Instance.DeleteLobbyAsync(_currentLobby.Id);
                 else await Lobbies.Instance.RemovePlayerAsync(_currentLobby.Id, Authentication.PlayerId);
                 _currentLobby = null;
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 Debug.Log(e);
             }
     }
+}
+
+public class Constants
+{
+    public const string JoinKey = "j";
+    public const string DifficultyKey = "d";
+    public const string GameTypeKey = "t";
+
+    public const int MAX_PLAYER = 2;
+
+    public static readonly List<string> GameTypes = new() { "Normal", "Friends", "Rank" };
+
+    public static readonly List<string> Difficulties = new() { "None", "Basic", "Medium", "Hard", "Extreme", "Nightmare" };
+    // None: for Friends, Normal
+    // Basic: for Rank 1 - 2
+    // Medium: for Rank 3 - 4
+    // Hard: for Rank 5
+    // Extreme: for Rank 6
+    // Nightmare: for Rank 7
+}
+
+public struct LobbyData
+{
+    public int MaxPlayers;
+    public int Difficulty;
+    public int Type;
 }
