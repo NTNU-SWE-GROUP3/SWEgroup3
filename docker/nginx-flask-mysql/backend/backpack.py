b672a0254e2cc37186fd40acebb66fc433794fbb
emeswee <miswij@gmail.com>
2023-12-07 03:11:03 +0800

    feat: skill display backend

--- docker/nginx-flask-mysql/backend/backpack.py
+++ docker/nginx-flask-mysql/backend/backpack.py
@@ -31,6 +31,8 @@ class BPManager:
         else:
             return -1
         
+    ############## CARD STYLES #################
+
     def GetCardStyle(self, token_id, card_style_id):
         current_app.logger.info("bp acc id: " +  token_id)
         current_app.logger.info("bp card style id: "+ card_style_id)
@@ -97,17 +99,80 @@ class BPManager:
         current_app.logger.info("result: ", card_style_ids)
 
         return card_style_ids
-
     
-    # def SellCardStyle(self, accountID, targetCardStyleID):
-    #     account_id = self.FindAccountId(accountID)
-    #     if(account_id is None):
-    #         return False #account ID not found
-    #     delete_stmt = (
-    #         "DELETE FROM account_card_style "
-    #         'WHERE account_id = %s AND card_style_id = %s'
-    #     )
-    #     data = (accountID, targetCardStyleID)
-    #     self.cursor.execute(delete_stmt, data)
-    #     self.connection.commit()
+    ############## SKILL CARD #################
 
+    def GetSkillStyle(self, token_id, skill_id):
+        #account_id = int(token_id)
+        current_app.logger.info("bp acc id: ",  token_id)
+        current_app.logger.info("bp skill id: ", skill_id)
+
+         # Check if skill_id is not None before converting to int
+        if skill_id is not None:
+            skill_id = int(skill_id)
+        else:
+            # Handle the case where skill_id is None
+            return -1 
+        #skill_id = int(skill_id)
+        
+        selectInventoryStmt = (
+            "SELECT acs.skill_id "
+            "FROM account a "
+            "JOIN account_skill acs ON a.id = acs.account_id "
+            "WHERE a.token_id = %s AND acs.skill_id = %s "
+        )
+        # current_app.logger.info(selectInventoryStmt)
+        inventoryData = (token_id, skill_id)
+        self.cursor.execute(selectInventoryStmt, inventoryData)
+        rec = []
+        for c in self.cursor:
+            rec.append(c[0]) 
+        if(bool(rec)):
+            current_app.logger.info("rec : " , rec)
+            return rec[0]
+        else:
+            return -1
+        
+        
+    def EquipSkillStyle(self, token_id, skillId):
+        # account_id = self.FindAccountId(accountID)
+        # if(account_id == -1):
+        #     return False #account ID not found
+        update_stmt = (
+            "UPDATE account_skill acs "
+            "JOIN account a ON a.id = acs.account_id "
+            "SET acs.equip_status = 1 "
+            "WHERE a.token_id = %s AND acs.skill_id = %s"
+        )
+
+        updateData = (token_id, skillId)
+        self.cursor.execute(update_stmt, updateData)
+        self.connection.commit()
+
+        return True
+
+    def DisplaySkillStyle(self, token_id):
+        #current_app.logger.info("input skill id: ", skill_id)
+         # Create a comma-separated string of placeholders for the IN clause
+        #placeholders = ', '.join(['%s'] * len(skill_id))
+
+        select_stmt = (
+            "SELECT acs.skill_id "
+            "FROM account a "
+            "JOIN account_skill acs ON a.id = acs.account_id "
+            "WHERE a.token_id = %s"
+        )
+
+        # Combine the token_id and skill_id into a single tuple
+        #params = (token_id,) + tuple(skill_id)
+
+        self.cursor.execute(select_stmt, (token_id,))
+
+        # Fetch all the results
+        results = self.cursor.fetchall()
+
+        # Extract and return the skill_id values
+        skill_style_ids = [result[0] for result in results]
+        current_app.logger.info("result: ", skill_style_ids)
+
+        return skill_style_ids
