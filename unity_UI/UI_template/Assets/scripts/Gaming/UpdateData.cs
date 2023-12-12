10f0b10abb6a234e509271a463699c66e911601f
hsiaooooooo <alllllvin21292@gmail.com>
2023-12-12 23:11:58 +0800

    update game finish

--- unity_UI/UI_template/Assets/scripts/Gaming/UpdateData.cs
+++ unity_UI/UI_template/Assets/scripts/Gaming/UpdateData.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-
 public class UpdateData : MonoBehaviour
 {
-    private DontDestroy userdata;
-    int playerId = 0;
     string apiUrl = "http://140.122.185.169:5050/gaming/game_finish";
-    string response = "";
+    private DontDestroy userdata;
+    int playerId;
+    string response;
 
-
-    public void UpdateUserData()
+    public void SetUserdata()
     {
-        Debug.Log("WinOrLose: " + GameController.WinOrLose);
-        // StartCoroutine(SendRequest());
+        StartCoroutine(SendRequest(playerId.ToString(), GameController.WinOrLose));
     }
-    public IEnumerator SendRequest()
+
+    void Init()
+    {
+        userdata = FindObjectOfType<DontDestroy>();
+        playerId = PlayerPrefs.GetInt("id");
+    }
+    void Start()
+    {
+        Init();
+        Debug.Log(GameController.WinOrLose);
+    }
+
+    IEnumerator SendRequest(string playerId,string status)
     {
         WWWForm form = new WWWForm();
 
-        form.AddField("account_id",  playerId.ToString());
-        form.AddField("end_status", GameController.WinOrLose);
+        form.AddField("account_id", playerId);
+        form.AddField("end_status",status);
 
         UnityWebRequest www = UnityWebRequest.Post(apiUrl, form);
         yield return www.SendWebRequest();
-
 
         if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
         {
@@ -37,17 +45,5 @@ public class UpdateData : MonoBehaviour
             response = www.downloadHandler.text;
             Debug.Log(response);
         }
-    }
-
-    void Init()
-    {
-        userdata = FindObjectOfType<DontDestroy>();
-        playerId = PlayerPrefs.GetInt("id");
-        Debug.Log(playerId);
-    }
-    void Start()
-    {
-        Init();
-        // UpdateUserData();
     }
 }
